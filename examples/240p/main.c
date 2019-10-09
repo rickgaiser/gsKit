de8a7e5ce137481c7a7f6da14fa937cc1ca775e5
Maximus32 <rgaiser@gmail.com>
2019-10-09 22:16:46 +0200

    1080i with crt scanline 'shader'

--- examples/240p/main.c
+++ examples/240p/main.c
@@ -18,34 +18,54 @@
 #include <gsToolkit.h>
 
 
+#define HIRES_MODE
+
+void create_tex_effect(GSTEXTURE *eff, u32 height) {
+	u32 x,y;
+	u32 *pPixel;
+
+	eff->Width = 64; // Minimum width?
+	eff->Height = height;
+	eff->PSM = GS_PSM_CT32;
+	eff->Mem = malloc(gsKit_texture_size_ee(eff->Width, eff->Height, eff->PSM));
+	eff->Vram = 0;
+	eff->Filter = GS_FILTER_NEAREST;
+	gsKit_setup_tbw(eff);
+	pPixel = eff->Mem;
+
+	for(y=0; y<eff->Height; y++) {
+		for(x=0; x<eff->Width; x++) {
+			*pPixel++ = (y&1) ? GS_SETREG_RGBA(0x00,0x00,0x00,0x00) : GS_SETREG_RGBA(0x00,0x00,0x00,0x80);
+		}
+	}
+}
+
+
 int main(int argc, char *argv[])
 {
 	GSGLOBAL *gsGlobal = gsKit_init_global();
 	GSFONTM *gsFontM = gsKit_init_fontm();
 	GSTEXTURE tex;
+	GSTEXTURE tex_effect;
 	char tempstr[256];
 	int i=0;
 	int toggle=0;
-	unsigned int iScale;
+	unsigned int iScaleW;
+	unsigned int iScaleH;
 
 	u64 Black = GS_SETREG_RGBA(0x00,0x00,0x00,0x80);
 	u64 White = GS_SETREG_RGBA(0xFF,0xFF,0xFF,0x80);
 	u64 TexCol = GS_SETREG_RGBA(0x80,0x80,0x80,0x80);
 
-	gsGlobal->Mode = GS_MODE_DTV_480P;
-	gsGlobal->Interlace = GS_NONINTERLACED;
+	gsGlobal->Mode = GS_MODE_DTV_1080I;
+	gsGlobal->Interlace = GS_INTERLACED;
 	gsGlobal->Field = GS_FRAME;
-#if 1
-	gsGlobal->Width = 640;
-	gsGlobal->Height = 480;
-	iScale = 2;
-#else
-	gsGlobal->Width = 320;
-	gsGlobal->Height = 240;
-	iScale = 1;
-#endif
+	gsGlobal->Width = 1920;
+	gsGlobal->Height = 540;
+	iScaleW = 4;
+	iScaleH = 2;
 
-	gsGlobal->PSM = GS_PSM_CT24;
+	gsGlobal->PSM = GS_PSM_CT16;
 	gsGlobal->PSMZ = GS_PSMZ_16S;
 	gsGlobal->DoubleBuffering = GS_SETTING_ON;
 	gsGlobal->ZBuffering = GS_SETTING_OFF;
@@ -56,12 +76,18 @@ int main(int argc, char *argv[])
 	// Initialize the DMAC
 	dmaKit_chan_init(DMA_CHANNEL_GIF);
 
+#ifdef HIRES_MODE
+	gsKit_hires_init_screen(gsGlobal, 3);
+#else
 	gsKit_init_screen(gsGlobal);
+#endif
+
 	gsKit_fontm_upload(gsGlobal, gsFontM);
 
 	// Load textures
 	tex.Delayed = 1;
 	gsKit_texture_png(gsGlobal, &tex, "host:240p_2.png");
+	create_tex_effect(&tex_effect, tex.Height * iScaleH);
 
 	gsKit_set_clamp(gsGlobal, GS_CMODE_CLAMP);
 	gsKit_set_test(gsGlobal, GS_ZTEST_OFF);
@@ -69,7 +95,11 @@ int main(int argc, char *argv[])
 	gsGlobal->PrimAlphaEnable = GS_SETTING_ON;
 
 	while(1) {
+#ifndef HIRES_MODE
+		// HIRES mode automatically clears the screen
+		// So only clear the screen in normal mode
 		gsKit_clear(gsGlobal, Black);
+#endif
 
 		i++;
 		if (i >= 60) {
@@ -83,15 +113,15 @@ int main(int argc, char *argv[])
 		float fOffsetY = -0.5f;
 		float fOffsetU = 0.0f;
 		float fOffsetV = 0.0f;
-		unsigned int iDispWidth  = tex.Width  * iScale;
-		unsigned int iDispHeight = tex.Height * iScale;
+		unsigned int iDispWidth  = tex.Width  * iScaleW;
+		unsigned int iDispHeight = tex.Height * iScaleH;
 
 		// Center on screen
 		float fPosX = (gsGlobal->Width  - iDispWidth)  / 2;
 		float fPosY = (gsGlobal->Height - iDispHeight) / 2;
 
-		//tex.Filter = GS_FILTER_NEAREST;
-		tex.Filter = toggle ? GS_FILTER_LINEAR : GS_FILTER_NEAREST;
+		//tex.Filter = toggle ? GS_FILTER_LINEAR : GS_FILTER_NEAREST;
+		tex.Filter = GS_FILTER_NEAREST;
 		gsKit_TexManager_bind(gsGlobal, &tex);
 		gsKit_prim_sprite_texture(gsGlobal, &tex,
 			fOffsetX+fPosX,             // X1
@@ -105,13 +135,33 @@ int main(int argc, char *argv[])
 			2,
 			TexCol);
 
+		if (toggle) {
+			gsKit_TexManager_bind(gsGlobal, &tex_effect);
+			gsKit_prim_sprite_texture(gsGlobal, &tex_effect,
+				fOffsetX+fPosX,             // X1
+				fOffsetY+fPosY,             // Y2
+				fOffsetU+0.0f,              // U1
+				fOffsetV+0.0f,              // V1
+				fOffsetX+fPosX+iDispWidth,  // X2
+				fOffsetY+fPosY+iDispHeight, // Y2
+				fOffsetU+tex_effect.Width,  // U2
+				fOffsetV+tex_effect.Height, // V2
+				2,
+				TexCol);
+		}
+
 		//sprintf(tempstr, "fOffsetXY = %.3f %.3f", fOffsetX, fOffsetY);
 		//gsKit_fontm_print_scaled(gsGlobal, gsFontM, 20, 400, 2, 0.5f, Black, tempstr);
 		//sprintf(tempstr, "fOffsetUV = %.3f %.3f", fOffsetU, fOffsetV);
 		//gsKit_fontm_print_scaled(gsGlobal, gsFontM, 20, 430, 2, 0.5f, Black, tempstr);
 
+#ifdef HIRES_MODE
+		gsKit_hires_sync(gsGlobal);
+		gsKit_hires_flip(gsGlobal);
+#else
 		gsKit_queue_exec(gsGlobal);
 		gsKit_sync_flip(gsGlobal);
+#endif
 		gsKit_TexManager_nextFrame(gsGlobal);
 	}
 
